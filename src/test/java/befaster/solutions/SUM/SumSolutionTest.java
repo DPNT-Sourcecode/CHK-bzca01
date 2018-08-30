c26ef95d72ca889207f5c7146e27fc3a58cc11a6
sbx_user1060 <sbx_user1060@ip-10-128-143-64.eu-west-2.compute.internal>
2018-08-30 16:30:03 +0000

    Thu Aug 30 16:30:03 UTC 2018

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -19,4 +19,49 @@ public class SumSolutionTest {
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testNullIntegerPassedInParam1(){
+        sum.sum(null, Integer.valueOf(1));
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testNullIntegerPassedInParam2(){
+        sum.sum(Integer.valueOf(1), null);
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testTooLowIntegerPassedInParam1(){
+        sum.sum(Integer.valueOf(-1), Integer.valueOf(1));
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testTooHighIntegerPassedInParam1(){
+        sum.sum(Integer.valueOf(101), Integer.valueOf(1));
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testTooLowIntegerPassedInParam2() {
+        sum.sum(Integer.valueOf(1), Integer.valueOf(-1));
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void testTooHighIntegerPassedInParam2(){
+        sum.sum(Integer.valueOf(1), Integer.valueOf(101));
+    }
+
+    @Test
+    public void testMedians(){
+        assertThat(sum.sum(Integer.valueOf(50),Integer.valueOf(50)), equalTo(Integer.valueOf(100)));
+    }
+
+    @Test
+    public void testLowBoundary(){
+        assertThat(sum.sum(Integer.valueOf(1),Integer.valueOf(1)), equalTo(Integer.valueOf(2)));
+    }
+
+    @Test
+    public void testHighBoundary(){
+        assertThat(sum.sum(Integer.valueOf(100),Integer.valueOf(100)), equalTo(Integer.valueOf(200)));
+    }
 }
