770d32f82832615ef3e0fc84474415c519d8893d
sbx_user1060 <sbx_user1060@ip-10-128-143-64.eu-west-2.compute.internal>
2018-08-30 16:59:32 +0000

    Thu Aug 30 16:59:32 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,14 +2,24 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.function.Predicate;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        skus.chars()
-                .filter(ch->Character.isAlphabetic(ch))
-                .filter()
+
+        long countOfProductA = countProduct(skus, 'A');
+        long countOfProductB = countProduct(skus, 'B');
+        long countOfProductC = countProduct(skus, 'C');
+        long countOfProductD = countProduct(skus, 'D');
+
 
         throw new SolutionNotImplementedException();
     }
 
-    private boolean
+
+    private long countProduct( String skus, final int productId ){
+        return skus.chars()
+                .filter(ch->ch==productId)
+                .count();
+    }
 }
