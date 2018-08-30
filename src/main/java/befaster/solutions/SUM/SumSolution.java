9a2ab17b0cc733127fa7e4c832ddd7ae9aa9b9fd
sbx_user1060 <sbx_user1060@ip-10-128-143-64.eu-west-2.compute.internal>
2018-08-30 16:15:03 +0000

    Thu Aug 30 16:15:03 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -2,10 +2,31 @@ package befaster.solutions.SUM;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Optional;
+
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        return x + y;
+    }
+
+    public Integer sum(Integer i1, Integer i2 ){
+        validInteger(0, i1 );
+    }
+
+    private void validInteger( int index, Optional<Integer> optInt ){
+        if( !optInt.isPresent() ){
+            throw new IllegalArgumentException( String.format("parameter %d cannot be null", index ));
+        }
+        else{
+            int iValue = optInt.get().intValue();
+            if( iValue < 0 ){
+                throw new IllegalArgumentException( String.format("parameter %d (%d) must be greater than or equal to 0", index, iValue ));
+            }
+            if( iValue > 100 ){
+                throw new IllegalArgumentException( String.format("parameter %d (%d) must be less than or equal to 100", index, iValue ));
+            }
+        }
     }
 
 }
