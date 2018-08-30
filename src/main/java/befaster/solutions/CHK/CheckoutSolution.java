3482632a2f5d813153194c38a57234407ba54216
sbx_user1060 <sbx_user1060@ip-10-128-143-64.eu-west-2.compute.internal>
2018-08-30 18:03:38 +0000

    Thu Aug 30 18:03:38 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,6 +2,9 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class CheckoutSolution {
@@ -10,21 +13,24 @@ public class CheckoutSolution {
     private static final int PRODUCT_B = 'B';
     private static final int PRODUCT_C = 'C';
     private static final int PRODUCT_D = 'D';
+    private static final int PRODUCT_E = 'E';
 
     private static final int UNIT_PRICE_A = 50;
     private static final int UNIT_PRICE_B = 30;
     private static final int UNIT_PRICE_C = 20;
     private static final int UNIT_PRICE_D = 15;
+    private static final int UNIT_PRICE_E = 40;
 
-    private static final int MULTIBUY_PRICE_A = 130;
-    private static final int MULTIBUY_PRICE_B = 45;
-    private static final int MULTIBUY_PRICE_C = 20;
-    private static final int MULTIBUY_PRICE_D = 15;
+    private static Map<Integer,Integer> MULTIBUYS_A = new HashMap<>();
+    private static Map<Integer,Integer> MULTIBUYS_B = new HashMap<>();
+    private static Map<Integer,Integer> MULTIBUYS_C = new HashMap<>();
+    private static Map<Integer,Integer> MULTIBUYS_D = new HashMap<>();
+    private static Map<Integer,Integer> MULTIBUYS_E = new HashMap<>();
 
-    private static final Integer MULTIBUY_THRESHOLD_A = 3;
-    private static final Integer MULTIBUY_THRESHOLD_B = 2;
-    private static final Integer MULTIBUY_THRESHOLD_C = null;
-    private static final Integer MULTIBUY_THRESHOLD_D = null;
+    public CheckoutSolution(){
+        MULTIBUYS_A.put(3,130);
+        MULTIBUYS_A.put(5,200);
+    }
 
     public Integer checkout(String skus) {
 
@@ -36,11 +42,22 @@ public class CheckoutSolution {
             long countOfProductB = countProduct(skus, PRODUCT_B);
             long countOfProductC = countProduct(skus, PRODUCT_C);
             long countOfProductD = countProduct(skus, PRODUCT_D);
+            long countOfProductE = countProduct(skus, PRODUCT_E);
 
-            checkoutTotal = priceProduct(countOfProductA, UNIT_PRICE_A, MULTIBUY_THRESHOLD_A, MULTIBUY_PRICE_A )
-                    + priceProduct(countOfProductB, UNIT_PRICE_B, MULTIBUY_THRESHOLD_B, MULTIBUY_PRICE_B )
-                    + priceProduct(countOfProductC, UNIT_PRICE_C, MULTIBUY_THRESHOLD_C, MULTIBUY_PRICE_C )
-                    + priceProduct(countOfProductD, UNIT_PRICE_D, MULTIBUY_THRESHOLD_D, MULTIBUY_PRICE_D );
+            long countFreeGifts = countOfProductE % 2;
+
+            if( countFreeGifts > 0 ){
+                countOfProductB = countOfProductB - countFreeGifts;
+
+                countOfProductE = countOfProductE / 2;
+            }
+
+
+            checkoutTotal = priceProduct(countOfProductA, UNIT_PRICE_A, MULTIBUYS_A )
+                    + priceProduct(countOfProductB, UNIT_PRICE_B, MULTIBUYS_B )
+                    + priceProduct(countOfProductC, UNIT_PRICE_C, MULTIBUYS_C )
+                    + priceProduct(countOfProductD, UNIT_PRICE_D, MULTIBUYS_D )
+                    + priceProduct(countOfProductE, UNIT_PRICE_D, MULTIBUYS_E );
 
         }
 
@@ -56,22 +73,29 @@ public class CheckoutSolution {
 
     private long countInvalidProducts( String skus ){
         return skus.chars()
-                .filter(ch->!(ch==PRODUCT_A || ch==PRODUCT_B || ch==PRODUCT_C || ch==PRODUCT_D) )
+                .filter(ch->!(ch==PRODUCT_A || ch==PRODUCT_B || ch==PRODUCT_C || ch==PRODUCT_D || ch==PRODUCT_E) )
                 .count();
     }
 
-    public Integer priceProduct( long count , Integer unitPrice, Integer multiBuyNumber, Integer multiBuyPrice ){
+    public Integer priceProduct( long count , Integer unitPrice, Map<Integer,Integer> multiBuys ){
         Integer price = Integer.valueOf(0);
 
         int iCount = (int)count;
 
         if( count > 0 ){
-            if( multiBuyNumber != null ){
-                int numMultiBuys = iCount / multiBuyNumber.intValue();
-                int nonQualifying = iCount % multiBuyNumber.intValue();
+            if( !multiBuys.isEmpty() ){
 
-                price = Integer.valueOf(numMultiBuys);
-                price = Integer.valueOf((numMultiBuys*multiBuyPrice.intValue()) + (nonQualifying*unitPrice.intValue()));
+                Integer[] intKeys = multiBuys.keySet().toArray(new Integer[]{});
+                Arrays.sort(intKeys);
+
+                for(int i = 0; i<intKeys.length; i++ ){
+                    int numMultiBuys = iCount / intKeys[i];
+                    int nonQualifying = iCount % intKeys[i];
+                    price = price.intValue() + multiBuys.get(intKeys);
+                    iCount = iCount - numMultiBuys;
+                }
+
+                price = price + (iCount*unitPrice.intValue());
             }
             else{
                 price = Integer.valueOf( iCount * unitPrice.intValue() );
